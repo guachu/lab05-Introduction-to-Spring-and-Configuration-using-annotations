c20f8f6995df67de80377349899a0f2e028eee30
juanmora98 <2110111@labinfo.is.escuelaing.edu.co>
2019-02-14 17:29:00 -0500

    Spring finalizado

---
+++ src/main/java/edu/eci/arsw/springdemo/SpellChecker.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+public interface SpellChecker {
+    
+    
+
+	public String checkSpell(String text);
+	
+}
